e02d0d4453d19861797f3daad33060a87d8590a3
Robert Krahn <robert.krahn@gmail.com>
2013-12-19 02:57:34 -0800

    adding vwf-specific localconfig.js

--- install-lively.sh
+++ install-lively.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -d LivelyKernel ]; then rm -rf LivelyKernel; fi
-
 cd /vagrant/
+if [ -d LivelyKernel ]; then rm -rf LivelyKernel; fi
 git clone -b vwf-compat https://github.com/LivelyKernel/LivelyKernel
 cd /vagrant/LivelyKernel
-ln -s $PWD $HOME/LivelyKernel
+ln -fs $PWD $HOME/LivelyKernel
 npm install
+
+# setup localconfig, required for vwf right now
+conf=$PWD/core/lively/localconfig.js
+touch $conf
+curl https://gist.github.com/rksm/8037313/raw/a6d52180305f027037ffcb16350d50ded072d5c6/gistfile1.js > $conf
